5905f43f4c005d8519e7daabacb3fcf2ea13a106
zerocool <one1970zero@mail.ru>
2023-09-26 23:47:54 +0300

    add firestore and questions

--- app/src/main/java/ru/malohelm/quizapp/activities/MainActivity.kt
+++ app/src/main/java/ru/malohelm/quizapp/activities/MainActivity.kt
@@ -2,9 +2,13 @@ package ru.malohelm.quizapp.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.MenuItem
+import android.widget.Toast
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.recyclerview.widget.GridLayoutManager
+import com.google.firebase.firestore.CollectionReference
+import com.google.firebase.firestore.FirebaseFirestore
 import ru.malohelm.quizapp.R
 import ru.malohelm.quizapp.adapters.QuizAdapter
 import ru.malohelm.quizapp.data.entity.Quiz
@@ -13,8 +17,9 @@ import ru.malohelm.quizapp.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private lateinit var actionBarDrawerToggle: ActionBarDrawerToggle
-    lateinit var adapter: QuizAdapter
+    private lateinit var adapter: QuizAdapter
     private var quizList = mutableListOf<Quiz>()
+    private lateinit var firestore: FirebaseFirestore
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -40,8 +45,21 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun setUpViews(){
+        setUpFireStore()
         setUpDrawerLayout()
         setUpRecyclerView()
+    }
+
+    private fun setUpFireStore() {
+        firestore = FirebaseFirestore.getInstance()
+        val collectionReference: CollectionReference = firestore.collection("quizapp")
+        collectionReference.addSnapshotListener { value, error ->
+            if (value == null || error == null){
+                Toast.makeText(this, "Error fetching data", Toast.LENGTH_SHORT).show()
+                return@addSnapshotListener
+            }
+            Log.d("DATA", value.toObjects(Quiz::class.java).toString())
+        }
     }
 
     private fun setUpRecyclerView(){
